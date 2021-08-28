e2f1ae5cf212046a5173e3048df94c540b79271d
AutoLogon <msadrolodabaee@myseneca.ca>
2021-08-28 03:17:11 -0400

    making Weather.js to fetch from api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import React from 'react';
 
+import { Weather } from './Weather.js'
 
-
-
+const data = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=133d63b6fb36bb4cfa302d11a24ad289';
+console.log(data.main)
 
 function App() {
   return (
     <div className="">
-      hellollll
+      hellollll<br />
+
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,21 @@
-import React from 'react';
+
+
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = 'api.openweathermap.org/data/2.5/weather?';
 
-function Weather() {
-    return (
-        <div>
 
-        </div>
-    )
+
+
+const Weather = async (city) => {
+
+    try {
+        const { data } = await axios.get(`${BASE_URL}q=${city}& appid=${API_KEY}`)
+        return data;
+    } catch (err) { throw err; }
 }
 
 export default Weather;
