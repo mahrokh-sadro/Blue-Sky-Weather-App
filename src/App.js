8e4684471a8debaf47a3061b6e3d84ebc85fba92
AutoLogon <msadrolodabaee@myseneca.ca>
2021-08-28 02:27:07 -0400

    making Weather.js to fetch from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 
+import React from 'react';
+
+
+
+
+
 function App() {
   return (
     <div className="">
---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = 'api.openweathermap.org/data/2.5/weather?';
+
+function Weather() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Weather;
