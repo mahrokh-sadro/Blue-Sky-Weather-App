fa325356a75d88c66cfc6e0be1ae65872c456933
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-01-03 00:09:33 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -4,29 +4,34 @@ import React, { useState, useEffect } from 'react';
 import { Weather } from './Weather.js'
 
 import './App.css'
-
+const API_KEY = '133d63b6fb36bb4cfa302d11a24ad289'
+const BASE_URL = 'api.openweathermap.org/data/2.5/weather?';
 
 function App() {
-  const [city, setCity] = useState('Toronto');
+  const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
 
-  const getData = async () => {
-    try {
-      setIsLoading(true);
-      const data = await Weather(city);
-      setWeatherData(data);
-      setIsLoading(false);
-    }
-    catch (err) {
-      //err.message
-      setIsLoading(false);
-    }
-  }
 
   useEffect(() => {
-    getData();
+    console.log("city----->"+city)
+fetch(`${BASE_URL}q=${city}&appid=${API_KEY}`)
+.then(res=>{
+  console.log(  res)
+  res.json()
+  
+})
+.then(res=>{
+  console.log(  res)
+   setWeatherData(res);
+  console.log("weather------->"+weatherData.coord)
+
+})
+.catch(e=>console.log(e))
+
   }, [])
+
+
+
 
   return (
     <div className='app warm'>
@@ -36,7 +41,11 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="city"
+            value = {city}
+            onChange={e=>setCity(e.target.value)}
           />
+          <button type="submit">sub</button>
+          <p>{weatherData}</p>
         </div>
       </main>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,21 +1,21 @@
 
 
 
-import axios from 'axios';
+// import axios from 'axios';
 
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-const BASE_URL = 'api.openweathermap.org/data/2.5/weather?';
+// const API_KEY = '133d63b6fb36bb4cfa302d11a24ad289'
+// const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?';
 
 
 
 
-export const Weather = async (city) => {
+// export const Weather = async (city) => {
 
-    try {
-        const { data } = await axios.get(`${BASE_URL}q=${city}& appid=${API_KEY}`)
-        return data;
-    } catch (err) { throw err; }
-}
+//     try {
+//         const { data } = await axios.get(`${BASE_URL}q=${city}&appid=${API_KEY}`)
+//         return data;
+//     } catch (err) { throw err; }
+// }
 
 
