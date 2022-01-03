a12f2cfffcf243a1d1b52107f8b6f7a60132516b
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-01-03 00:52:03 -0500

    making changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,32 @@
 
 import React, { useState, useEffect } from 'react';
 
+
 import './App.css'
+
+
+
+const App = () => {
+
+const[city,setCity] = useState('')
+const[weather,setWeather] = useState(null);
+
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+    <main>
+      <div className="search-box">
+        <input 
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          onChange={e => setCity(e.target.value)}
+          value={city}
+          onKeyPress={onPressEnter}
+        />
+      </div>
+    </main>
+  </div>
+  );
+};
+
+export default App;
